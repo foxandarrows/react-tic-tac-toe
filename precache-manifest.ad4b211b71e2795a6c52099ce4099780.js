f416c2a37eafdbb1dc05759be7228d84f81a1157
saralitwak <sara.litwak@twipemobile.com>
2019-12-20 15:24:10 +0100

    Updates

---
+++ precache-manifest.ad4b211b71e2795a6c52099ce4099780.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e901ae8f0c7a432fcc0047b2b2ee74",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "894b40806bcc926699c4",
+    "url": "/react-tic-tac-toe/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/react-tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "894b40806bcc926699c4",
+    "url": "/react-tic-tac-toe/static/js/main.158dd89c.chunk.js"
+  },
+  {
+    "revision": "140d55aca7c0751087c1",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.a5f725ed.js"
+  }
+]);
